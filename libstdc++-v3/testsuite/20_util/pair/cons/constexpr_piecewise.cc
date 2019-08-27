99cb87c30619e289d2c72feb1b6f0b3bc5b9d97b
emsr <3dw4rd@verizon.net>
2019-08-27 11:50:10 -0400

    Push through constexpr tuple.  Add some testcases.

--- libstdc++-v3/testsuite/20_util/pair/cons/constexpr_piecewise.cc
+++ libstdc++-v3/testsuite/20_util/pair/cons/constexpr_piecewise.cc
@@ -19,25 +19,44 @@
 // <http://www.gnu.org/licenses/>.
 
 #include <utility>
+#include <tuple>
+
+template<typename Flt>
+  struct xy
+  {
+    int id;
+    Flt xv, yv;
+
+    constexpr xy(int iid, Flt ix, Flt iy)
+    : id{iid}, xv{ix}, yv{iy}
+    {}
+  };
+
+template<typename Flt>
+  struct z
+  {
+    int id;
+    Flt zv;
+
+    constexpr z(int iid, Flt iz)
+    : id{iid}, zv{iz}
+    {}
+  };
 
 constexpr bool
 test_pair()
 {
   auto ok = true;
 
-  std::pair<int, int> pi(1, 2);
-  std::pair<float, float> pf(3.4f, 5.6f);
-  pf = pi;
-  pf = std::move(pi);
+  using t1_t = std::tuple<int, double, double>;
+  const t1_t t1(0, 3.456, 6.789);
 
-  using t1f_t = std::tuple<int, float, float>;
-  const t1f_t t1f(0, 3.456f, 6.789f);
+  using t2_t = std::tuple<int, double>;
+  const t2_t t2(12, 3.142);
 
-  using t2f_t = std::tuple<int, float>;
-  const t2f_t t2f(12, 3.142f);
-  std::pair<t1f_t, t2f_t> p1f(std::piecewise_construct, t1f, t2f);
-  p1 = p1f;
-  p1 = std::move(p1f);
+  std::pair<t1_t, t2_t> p1(std::piecewise_construct, t1, t2);
+
+  std::pair<xy<double>, z<double>> p3(std::piecewise_construct, t1, t2);
 
   return ok;
 }
